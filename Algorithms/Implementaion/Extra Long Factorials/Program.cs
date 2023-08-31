28f1508e039a771a5f777ca2d9f37ffd09d5b0b2
vijay <vjvijay130@gmail.com>
2023-08-31 23:45:44 +0530

    Extra Long Factorials

---
+++ Algorithms/Implementaion/Extra Long Factorials/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class Result
+{
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n)
+    {
+        var fact = BigInteger.One;
+        for (int i = 1; i <= n; i++)
+        {
+            fact = fact * i;
+        }
+        Console.WriteLine(fact);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.extraLongFactorials(n);
+    }
+}
