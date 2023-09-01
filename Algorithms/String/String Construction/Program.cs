5120f5aab23f4965ba4ecd0f972b40832ae273f8
vijay <vjvijay130@gmail.com>
2023-09-01 23:48:04 +0530

    String Construction

---
+++ Algorithms/String/String Construction/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'stringConstruction' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int stringConstruction(string s)
+    {
+        s.ToCharArray();
+        HashSet<char> unique = new HashSet<char>(s);
+        return unique.Count();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            int result = Result.stringConstruction(s);
+        }
+
+    }
+}
