ae0926429099311cec9d644ece0c1789be4b4994
vijay <vjvijay130@gmail.com>
2023-09-14 22:29:00 +0530

    Missing Numbers

---
+++ Algorithms/Search/Missing Numbers/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'missingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY arr
+     *  2. INTEGER_ARRAY brr
+     */
+
+    public static List<int> missingNumbers(List<int> arr, List<int> brr)
+    {
+        List<int> response = new List<int>();
+        HashSet<int> unique = new HashSet<int>(brr);
+        var unarr = unique.ToArray();
+        for (int i = 0; i < unarr.Count(); i++)
+        {
+            int x = arr.Where(u => u == unarr[i]).ToList().Count();
+            int y = brr.Where(u => u == unarr[i]).ToList().Count();
+            if (y > x)
+            {
+                response.Add(unarr[i]);
+            }
+        }
+        response.Sort();
+        return response;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int m = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> brr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(brrTemp => Convert.ToInt32(brrTemp)).ToList();
+
+        List<int> result = Result.missingNumbers(arr, brr);
+
+        foreach(var data in result)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
