96c8043296e44f54640764fff512a58db02933cd
vijay <vjvijay130@gmail.com>
2023-09-16 23:25:22 +0530

    Sparse Arrays

--- Algorithms/String/High Value Palindrome/Program.cs
+++ Algorithms/String/High Value Palindrome/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static string twoStrings(string s1, string s2)
+    {
+        s1.ToCharArray();
+        s2.ToCharArray();
+        HashSet<char> unique1 = new HashSet<char>(s1);
+        HashSet<char> unique2 = new HashSet<char>(s2);
+        var un1 = unique1.ToArray();
+        var un2 = unique2.ToArray();
+        for (int i = 0; i < un1.Count(); i++)
+        {
+            if (un2.Contains(un1[i]))
+            {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s1 = Console.ReadLine();
+
+            string s2 = Console.ReadLine();
+
+            string result = Result.twoStrings(s1, s2);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Data Structures/Arrays/Sparse Arrays/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY stringList
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<int> matchingStrings(List<string> stringList, List<string> queries)
+    {
+        List<int> result = new List<int>();
+        for (int i = 0; i < queries.Count(); i++)
+        {
+            int x = 0;
+            for (int j = 0; j < stringList.Count(); j++)
+            {
+                if (string.Equals(stringList[j], queries[i]))
+                {
+                    x++;
+                }
+            }
+            result.Add(x);
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the arrays!");
+
+        int stringListCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> stringList = new List<string>();
+
+        for (int i = 0; i < stringListCount; i++)
+        {
+            string stringListItem = Console.ReadLine();
+            stringList.Add(stringListItem);
+        }
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> queries = new List<string>();
+
+        for (int i = 0; i < queriesCount; i++)
+        {
+            string queriesItem = Console.ReadLine();
+            queries.Add(queriesItem);
+        }
+
+        List<int> res = Result.matchingStrings(stringList, queries);
+        foreach (int i in res)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
