faacbbf80390ca99a99ee25b8d275536d5849e03
vijay <vjvijay130@gmail.com>
2023-09-17 23:49:10 +0530

    sparse arrays

---
+++ Data Structures/Arrays/Lefy Rotation/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'rotateLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<int> rotateLeft(int d, List<int> arr)
+    {
+        var q = new Queue<int>(arr);
+        while (d > 0)
+        {
+            int y = q.Peek();
+            q.Enqueue(y);
+            q.Dequeue();
+            d--;
+        }
+        return q.ToList();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int d = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.rotateLeft(d, arr);
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
