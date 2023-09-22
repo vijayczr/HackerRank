63b4be719be6163acfee172a134929e93d128193
vijay <vjvijay130@gmail.com>
2023-09-22 23:58:16 +0530

    Dynamic Array

---
+++ Data Structures/Arrays/Dynamic Array/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'dynamicArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static List<int> dynamicArray(int n, List<List<int>> queries)
+    {
+        var arr = new List<List<int>>();
+        while (arr.Count() != n)
+        {
+            var d = new List<int>();
+            arr.Add(d);
+        }
+        int lastint = 0;
+        List<int> ansers = new List<int>();
+        foreach (var data in queries)
+        {
+            if (data[0] == 1)
+            {
+                var dx = (data[1] ^ lastint) % n;
+                arr[dx].Add(data[2]);
+            }
+            else if (data[0] == 2)
+            {
+                var dx = (data[1] ^ lastint) % n;
+                var x = arr[dx][data[2] % arr[dx].Count()];
+                lastint = x;
+                ansers.Add(x);
+            }
+        }
+        return ansers;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int q = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<List<int>> queries = new List<List<int>>();
+
+        for (int i = 0; i < q; i++)
+        {
+            queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+        }
+
+        List<int> result = Result.dynamicArray(n, queries);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
